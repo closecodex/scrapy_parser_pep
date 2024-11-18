cd541b89130dcca12bf212160c470c6f0fe9b7de
closecodex <closecodex@gmail.com>
2024-11-18 20:12:16 +0000

    edited file

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -8,7 +8,9 @@ class PepSpider(scrapy.Spider):
     start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
-        pep_links = response.xpath('//a[contains(@class, "pep reference internal")]/@href').extract()
+        pep_links = response.xpath(
+            ('//a[contains(@class, "pep reference internal")]/@href').extract()
+        )
         for link in pep_links:
             yield response.follow(link, callback=self.parse_pep)
 
@@ -16,8 +18,10 @@ class PepSpider(scrapy.Spider):
         title_parts = response.css('.page-title::text').get().split()
         pep_number = title_parts[1]
         pep_name = ' '.join(title_parts[3:])
-        status = response.xpath('//dt[contains(text(), "Status")]/following-sibling::dd[1]/abbr/text()').get()
-
+        status = response.xpath(
+            '//dt[contains(text(), "Status")]'
+            '/following-sibling::dd[1]/abbr/text()'
+        ).get()
         yield PepParseItem(
             number=int(pep_number),
             name=pep_name,
